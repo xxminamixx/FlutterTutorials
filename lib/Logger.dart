227ebd5a14e4bd89f86b1560c2fc3e3c4f73a0c2
kyohei.minami <pencil3215@gmail.com>
2019-10-05 19:21:41 +0900

    Dartの言語使用を学習するために基本的なコードを実装 * クラスの書き方 * Enumの書き方 * Interfaceの実装方法

---
+++ lib/Logger.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Logger {
+  static void debug(message) {
+    print(message);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/Logger.dart';
 
 void main() {
   // MyAppクラスを実行する
@@ -10,6 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    Logger.debug(addInt(10, 20));
+    Logger.debug(addString("あああ", "いいい"));
+    Logger.debug(addDynamic(10, 9));
+    Logger.debug(addDynamic("ジェネリクス", "で足し算"));
+    
     return new MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
@@ -24,4 +30,41 @@ class MyApp extends StatelessWidget {
     );
   }
 
+  /// Intの足し算関数
+  int addInt(int value1, int value2) {
+    return value1 + value2;
+  }
+
+  /// 文字列の連結
+  String addString(String value1, String value2) {
+      return value1 + value2;
+  }
+
+  /// ジェネリクスな足し算メソッド
+  addDynamic(value1, value2) {
+    return value1 + value2;
+  }
+
+}
+
+// MARK: enum: 列挙型。型は定義できない
+enum ErrorType {
+  TIMEOUT,
+  STANDARD
+}
+
+// MARK: abstract: 抽象度の高いクラスを表す。()でインスタンス生成できない
+abstract class Greeting {
+  void hello(value) {
+    print("Hello, $value");
+  }
+}
+
+// MARK: implements: インターフェースとして採用・extends: 継承
+
+/// Greetingを採用したクラス
+class Human implements Greeting {
+  void hello(value) {
+    print("Goodbye, $value");
+  }
 }
